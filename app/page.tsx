361d4c53b3ccc2fc2f23d29f47b44295ca61732e
Dev634 <pablolola@live.fr>
2026-04-06 15:45:07 +0200

    chore : change typos in HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-export default function Home() {
+export default function HomePage() {
   return (
       <main className="min-h-screen p-8">
         <div className="mx-auto max-w-4xl space-y-4">
           <h1 className="text-3xl font-bold">Fullstack Starter</h1>
-          <p className="text-sm text-zinc-600"> Semaine 1 : Next js + Prisma + Docker Compose + Postgresql + Github CI</p>
+          <p className="text-sm text-zinc-600"> Semaine 1 : Next.js + Prisma + Docker Compose + PostgreSQL + GitHub CI</p>
         </div>
       </main>
 
