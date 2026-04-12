cabfbfdc2c2ab3d372b4d5ce13bcf34321619b29
Dev634 <pablolola@live.fr>
2026-04-12 15:19:37 +0200

    create title

---
+++ components/Title.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Title({ title, as }: { title: string, as?: React.ElementType }) {
+  const Heading = as || "h1";
+  return (
+    <Heading className="text-3xl font-bold">{title}</Heading>
+  );
+}
