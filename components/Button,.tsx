d98adb207a6c8360388950da4c1fbaa9833249db
Dev634 <pablolola@live.fr>
2026-04-12 17:15:21 +0200

    test link button 2

--- components/Button,.tsx
+++ components/Button,.tsx
@@ -14,14 +14,14 @@ export default function Button({...props}: ButtonProps<ButtonTypeEnum>) {
   const buttonType = props.as === "submit" ? "submit" : "button";
   if (props.as === "link") {
     return (
-      <Link href={props.href} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+      <Link href={props.href} className="w-full px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
         {props.text}
       </Link>
     );
   }
   
   return (
-    <button type={buttonType} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+    <button type={buttonType} className="w-full px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
       {props.text}
     </button>
   );
