d8345dc45037db42ca4db3aa1ec98be666286b0c
Dev634 <pablolola@live.fr>
2026-04-12 19:13:48 +0200

    created AddClientForm

--- app/clients/add/page.tsx
+++ app/clients/add/page.tsx
@@ -2,7 +2,7 @@ import AddClientForm from "@/forms/AddClientForm";
 
 export default function AddClientPage(){
     return (
-        <main className="min-h-screen py-8">
+        <main className="flex flex-col justify-center min-h-screen py-8">
             <div className="w-8/12 mx-auto space-y-4">
                 <h1 className="text-3xl font-bold mb-6">Add Client</h1>
                 <AddClientForm />
