95976190df9eb4c87d90289d0bb7776db7390f18
Dev634 <pablolola@live.fr>
2026-04-12 16:55:43 +0200

    test link button

--- app/clients/page.tsx
+++ app/clients/page.tsx
@@ -7,9 +7,9 @@ export default function ClientsPage() {
       <main className="flex justify-center min-h-screen py-8">
         <div className="container w-8/12 space-y-4 py-8">
           <Title title="Clients" />
-          <ul className="space-y-4">
+          <ul className="mb-4 space-y-4">
             {[{ id: 1, name: "Client 1" }, { id: 2, name: "Client 2" }, { id: 3, name: "Client 3" }].map((client) => (
-                <li key={client.id} className="flex border rounded mb-4 cursor-pointer hover:bg-gray-100 hover:text-gray-700">
+                <li key={client.id} className="flex border rounded cursor-pointer hover:bg-gray-100 hover:text-gray-700">
                   <Link href={`/clients/${client.id}`} className="px-8 py-4 w-full text-lg font-semibold">
                     {client.name}
                   </Link>
