be39d070f20cb70efc37aa97ad00a21c6ba9c017
Dev634 <pablolola@live.fr>
2026-04-12 18:57:14 +0200

    test link button 2

--- components/Button,.tsx
+++ components/Button,.tsx
@@ -1,27 +1,34 @@
 import Link from "next/link";
- 
-type ButtonTypeEnum = "button" | "link" | "submit";
-type ButtonProps<T extends ButtonTypeEnum> = T extends "link" ? {
-    text: string;
-    href: string;
-    as?: T;
-} : {
-    text: string;
-    as?: T;
-};  
 
-export default function Button({...props}: ButtonProps<ButtonTypeEnum>) {
+type ButtonTypeEnum = "button" | "link" | "submit";
+type ButtonProps<T extends ButtonTypeEnum> = T extends "link"
+  ? {
+      text: string;
+      href: string;
+      as?: T;
+    }
+  : {
+      text: string;
+      as?: T;
+    };
+export default function Button(props: ButtonProps<ButtonTypeEnum>) {
   const buttonType = props.as === "submit" ? "submit" : "button";
   if (props.as === "link") {
     return (
-      <Link href={props.href} className="w-full px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+      <Link
+        href={props.href || ""}
+        className="w-full px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+      >
         {props.text}
       </Link>
     );
   }
-  
+
   return (
-    <button type={buttonType} className="w-full px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+    <button
+      type={buttonType}
+      className="w-full px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+    >
       {props.text}
     </button>
   );
