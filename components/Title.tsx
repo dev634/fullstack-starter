e5984670a8797c610fcf20b2137067e6c2c616a9
Dev634 <pablolola@live.fr>
2026-05-01 15:09:17 +0200

    change addFormClient

--- components/Title.tsx
+++ components/Title.tsx
@@ -3,6 +3,6 @@ import React from "react";
 export default function Title({ title, as }: { title: string, as?: React.ElementType }) {
   const Heading = as || "h1";
   return (
-    <Heading className="text-3xl font-bold">{title}</Heading>
+    <Heading className="text-3xl font-bold mb-6">{title}</Heading>
   );
 }
