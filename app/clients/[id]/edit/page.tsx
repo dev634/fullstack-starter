4c3e4cb66036be19ddc6f2c3c046b76ebb5d939f
Dev634 <pablolola@live.fr>
2026-06-07 11:46:08 +0200

    add formDtat helpers

--- app/clients/[id]/edit/page.tsx
+++ app/clients/[id]/edit/page.tsx
@@ -1,8 +1,9 @@
 import Title from "@/components/Title";
-
+import UpdateClientForm from "@/forms/UpdateClientForm";
 
 export default function EditPage(){
     return <main className="flex flex-col justify-center items-center h-dvh overflow-y-auto pb-8">
                 <Title title="Edit page"/>
+                <UpdateClientForm />
            </main>
 }
---
+++ forms/UpdateClientForm.tsx
@@ -0,0 +1,127 @@
+'use client'
+import { addClient } from "@/actions/clients/clients";
+import { useEffect, useActionState, useState } from 'react';
+import { Client } from "@/app/generated/prisma/client";
+import { Input } from "@/components/Inputs";
+import { Toast } from "@/components/Toast";
+import {useRouter} from "next/navigation";
+
+
+type AddClientFormState =
+  | { type: "success"; message: string }
+  | { type: "error"; message: string }
+  | { type: "zodError"; message: string; fieldsForm: Partial<Omit<Client, "id">>}
+  | { type: null; message: string };
+
+const initialState: AddClientFormState = {
+  type: null,
+  message: "",
+}
+
+export default function AddClientForm() {
+  const router = useRouter();
+  const [state, formAction, isPending] = useActionState<AddClientFormState, FormData>(
+    addClient,
+    initialState
+  );
+  const [values, setValues] = useState<Omit<Client, "id">>({
+    firstName: "",
+    lastName: "",
+    email: "",
+    companyName: "",
+    address: "",
+    country: "",
+    city: "",
+    zipCode: ""
+  });
+  const [feedback, setFeedback] = useState<AddClientFormState>(initialState);
+
+  useEffect(() => {
+    if (state.type !== null) {
+      setFeedback(state);
+      const timer = setTimeout(() => {
+        setFeedback(initialState);// Reset form by changing its key
+        router.push("/clients"); // Redirect to clients list after adding a client
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [state]);
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setValues(prev => ({ ...prev, [name]: value }));
+  }
+
+  return (
+    <form action={formAction} className="w-full bg-transparent rounded shadow">
+      <Toast type={feedback.type} message={feedback.message} />
+      <Input
+        label="Firstname"
+        name="firstName"
+        onChange={handleChange}
+        error={feedback.type === "zodError" ? feedback.fieldsForm : undefined}
+        value={values.firstName}
+      />
+      <Input
+        label="Lastname"
+        name="lastName"
+        onChange={handleChange}
+        error={feedback.type === "zodError" ? feedback.fieldsForm : undefined}
+        value={values.lastName}
+      />
+      <Input
+        label="Email"
+        name="email"
+        type="email"
+        onChange={handleChange}
+        error={feedback.type === "zodError" ? feedback.fieldsForm : undefined}
+        value={values.email}
+      />
+      <Input
+        label="Company Name"
+        name="companyName"
+        onChange={handleChange}
+        error={feedback.type === "zodError" ? feedback.fieldsForm : undefined}
+        value={values.companyName}
+      />
+      <Input
+        label="Address"
+        name="address"
+        onChange={handleChange}
+        error={feedback.type === "zodError" ? feedback.fieldsForm : undefined}
+        value={values.address}
+      />
+      <Input
+        label="Country"
+        name="country"
+        onChange={handleChange}
+        error={feedback.type === "zodError" ? feedback.fieldsForm : undefined}
+        value={values.country}
+      />
+      <Input
+        label="City"
+        name="city"
+        onChange={handleChange}
+        error={feedback.type === "zodError" ? feedback.fieldsForm : undefined}
+        value={values.city}
+      />
+      <Input
+        label="Zip Code"
+        name="zipCode"
+        onChange={handleChange}
+        error={feedback.type === "zodError" ? feedback.fieldsForm : undefined}
+        value={values.zipCode}
+      />
+      <button
+        type="submit"
+        disabled={isPending}
+        className={`w-full px-4 py-2 bg-blue-500 text-white 
+        rounded hover:bg-blue-600 cursor-pointer ${
+          isPending ? "opacity-50 cursor-not-allowed" : ""
+        }`}
+      >
+        Add Client
+      </button>
+    </form>
+  );
+}   
